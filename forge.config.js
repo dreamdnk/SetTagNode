8fe4f11ce5bb3bf07d6fc2c6093b4482190eab7e
zeronly <1163653517@qq.com>
2023-03-08 11:10:17 +0800

    Signed-off-by: zeronly <1163653517@qq.com>

---
+++ forge.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  packagerConfig: {
+	  name:"TagSetNote",
+	  icon:"favicon.ico"
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-zip'
+    }
+  ],
+};
